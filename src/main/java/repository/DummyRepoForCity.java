48fce0311531573e13615fb9c8bb50c7cc3ca4f5
rahulmayuran <rahulmayuran1996@gmail.com>
2021-02-17 17:10:10 +0530

    Added more comments and re-inserted city object of static block

--- src/main/java/repository/DummyRepoForCity.java
+++ src/main/java/repository/DummyRepoForCity.java
@@ -7,16 +7,20 @@ import org.hibernate.cfg.Configuration;
 
 import Entity.City;
 
-/*
- * Persisting the City object which is 
- * 1. Defined as a static variable
- * 2. Initialized using setters
- * 3. persisted in Database by save method
- */
-public class DummyRepoForCity {
+	/*
+	 * Persisting the City object which is 
+	 * 1. Defined as a static variable
+	 * 2. Initialized using setters
+	 * 3. persisted in Database by save method
+	 */
+	public class DummyRepoForCity {
 
 	static City cityobjblock;
 	
+	/*
+	 * Code inside static block is executed only once: the first time the class is loaded into memory
+	 * Static blocks are executed before constructors.
+	 */
 	static {
 		
 		cityobjblock = new City();
@@ -52,9 +56,16 @@ public class DummyRepoForCity {
 		
 		Transaction transactionObj = sessionObj.beginTransaction();
 		
-		//
+		//Persisting city object which is defined in static method of City type
 		sessionObj.save(CitySetter(emptycity));
+		
+		//Persisting city object which is instantiated and NULL values reflect in DB
 		sessionObj.save(emptycity);
+		
+		//Persisting city object which is declared in static block/variable of City type
+		sessionObj.save(cityobjblock);
+		
+		//WON'T WORK- Persisting city object which is declared in static block/variable of City type
 		sessionObj.save(cityobjblock);
 		
 		transactionObj.commit();
